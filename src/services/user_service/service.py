df8517f06b4d88eca9047659f1c15ad27de461f2
Konstantin Balandin <kostabalandin169@gmail.com>
2026-08-16 20:20:48 +0700

    updated user service

--- src/services/user_service/service.py
+++ src/services/user_service/service.py
@@ -1,2 +1,9 @@
+
 class UserService:
-    pass
+    def get_user(self, user_id: int):
+        if user_id < 1:
+            raise ValueError("User ID must be > 0")
+        # Этот блок не протестирован!
+        if user_id > 100:
+            return {"error": "User not found"}
+        return {"id": user_id, "name": f"User {user_id}"}
