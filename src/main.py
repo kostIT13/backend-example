65f66455960789f913ce015f60c5f5af53eeb68e
Konstantin Balandin <kostabalandin169@gmail.com>
2026-08-16 12:42:31 +0700

    include routers

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
+from src.api.routers.health_routers import router as health_routers
+from src.api.routers.users_routers import router as user_routers
 
 # 1. Создаём экземпляр приложения
 app = FastAPI(title="My API", version="0.1.0")
+
+app.include_router(health_routers)
+app.include_router(user_routers)
 
 # 2. Корневой эндпоинт
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
-
-# 3. Эндпоинт для проверки здоровья
-@app.get("/health")
-async def health_check():
-    return {"status": "healthy"}
